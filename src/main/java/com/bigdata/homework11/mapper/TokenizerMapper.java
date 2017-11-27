98f639c77602ebc49c5b30921f09a46aca64294d
Efim_Golovin <feanoref@gmail.com>
2017-11-27 19:01:26 +0300

    Performance improvements.

--- src/main/java/com/bigdata/homework11/mapper/TokenizerMapper.java
+++ src/main/java/com/bigdata/homework11/mapper/TokenizerMapper.java
@@ -15,9 +15,12 @@ public class TokenizerMapper extends Mapper<LongWritable, Text, IntWritable, Tex
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String txt = value.toString();
         String[] lines = txt.split(" ");
+        IntWritable key1  = new IntWritable(1);
+        Text value1 = new Text();
         for(String line: lines){
             if(line.length() > 0){
-                context.write(new IntWritable(1), new Text(line));
+                value1.set(line);
+                context.write(key1, value1);
             }
         }
     }
